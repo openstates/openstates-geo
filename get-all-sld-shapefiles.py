e2a61d8cbdfc21c42539281bb112deaed800970e
Miles Watkins <miles.w.watkins@gmail.com>
2019-03-23 07:14:54 -0400

    Add a slight delay between requests to try avoiding rate limiting by Census servers

--- get-all-sld-shapefiles.py
+++ get-all-sld-shapefiles.py
@@ -5,15 +5,18 @@
 # needed and can only be installed as a Python package
 
 import requests
+import time
 import us
 
 
 # The Census download URLs are case-sensitive
 URL = 'https://www2.census.gov/geo/tiger/TIGER2018/SLD{chamber_uppercase}/tl_2018_{fips}_sld{chamber}.zip'
+SECONDS_DELAY_TO_AVOID_RATE_LIMITING = 3
 
 for state in us.STATES + [us.states.PR]:
     print("Fetching shapefiles for {}".format(state.name))
     for chamber in ['l', 'u']:
+        time.sleep(SECONDS_DELAY_TO_AVOID_RATE_LIMITING)
         fips = state.fips
         download_url = URL.format(fips=fips, chamber=chamber, chamber_uppercase=chamber.upper())
         data = requests.get(download_url).content
