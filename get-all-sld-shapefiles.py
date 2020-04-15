db1b8a24fa3802ef1b157d02f365201b3a072d91
James Turk <dev@jamesturk.net>
2020-04-15 13:52:45 -0400

    make script resumable, instead of fancy retry logic

--- get-all-sld-shapefiles.py
+++ get-all-sld-shapefiles.py
@@ -1,45 +1,32 @@
-#! /usr/bin/env python3
-
-import logging
-import time
+#!/usr/bin/env python3
+import os
 import zipfile
-
 import requests
 import us
 
-
-logger = logging.getLogger(__file__)
-logger.addHandler(logging.StreamHandler())
-logger.setLevel(logging.INFO)
-
-# The Census download URLs are case-sensitive
+# note: The Census download URLs are case-sensitive
 YEAR = "2019"
 URL = "https://www2.census.gov/geo/tiger/TIGER{year}/SLD{chamber_uppercase}/tl_{year}_{fips}_sld{chamber}.zip"
-MAX_ATTEMPTS = 3
-SECONDS_BEFORE_RETRY = 5
 
 for state in us.STATES + [us.states.PR]:
-    logger.info("Fetching shapefiles for {}".format(state.name))
+    print("Fetching shapefiles for {}".format(state.name))
 
     for chamber in ["l", "u"]:
         fips = state.fips
+
+        if os.path.exists(f"data/tl_{YEAR}_{fips}_sld{chamber}.shp"):
+            print(f"skipping {state} {fips} sld{chamber}")
+            continue
+
         download_url = URL.format(
             fips=fips, chamber=chamber, chamber_uppercase=chamber.upper(), year=YEAR
         )
 
-        attempts = 0
-        while attempts < MAX_ATTEMPTS:
-            response = requests.get(download_url)
-            if response.status_code not in [500, 503]:
-                break
-            else:
-                attempts += 1
-                time.sleep(SECONDS_BEFORE_RETRY)
+        response = requests.get(download_url)
 
         if response.status_code == 200:
-            filename = "./data/tl_2018_{fips}_sld{chamber}.zip".format(
-                fips=fips, chamber=chamber
-            )
+            filename = f"./data/tl_{YEAR}_{fips}_sld{chamber}.zip"
+
             # This _could_ all be done with a single file operation,
             # by using a `BytesIO` file-like object to temporarily hold the
             # HTTP response. However, that's less readable and maintainable,
