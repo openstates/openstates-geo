24aebdebc8bac6cb43751d26839dce891f9d692e
James Turk <dev@jamesturk.net>
2020-04-15 13:39:32 -0400

    add poetry, update get-all-sld

--- get-all-sld-shapefiles.py
+++ get-all-sld-shapefiles.py
@@ -13,16 +13,19 @@ logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.INFO)
 
 # The Census download URLs are case-sensitive
-URL = 'https://www2.census.gov/geo/tiger/TIGER2018/SLD{chamber_uppercase}/tl_2018_{fips}_sld{chamber}.zip'
+YEAR = "2019"
+URL = "https://www2.census.gov/geo/tiger/TIGER{year}/SLD{chamber_uppercase}/tl_{year}_{fips}_sld{chamber}.zip"
 MAX_ATTEMPTS = 3
 SECONDS_BEFORE_RETRY = 5
 
 for state in us.STATES + [us.states.PR]:
     logger.info("Fetching shapefiles for {}".format(state.name))
 
-    for chamber in ['l', 'u']:
+    for chamber in ["l", "u"]:
         fips = state.fips
-        download_url = URL.format(fips=fips, chamber=chamber, chamber_uppercase=chamber.upper())
+        download_url = URL.format(
+            fips=fips, chamber=chamber, chamber_uppercase=chamber.upper(), year=YEAR
+        )
 
         attempts = 0
         while attempts < MAX_ATTEMPTS:
@@ -34,18 +37,21 @@ for state in us.STATES + [us.states.PR]:
                 time.sleep(SECONDS_BEFORE_RETRY)
 
         if response.status_code == 200:
-            filename = './data/tl_2018_{fips}_sld{chamber}.zip'.format(fips=fips, chamber=chamber)
+            filename = "./data/tl_2018_{fips}_sld{chamber}.zip".format(
+                fips=fips, chamber=chamber
+            )
             # This _could_ all be done with a single file operation,
             # by using a `BytesIO` file-like object to temporarily hold the
             # HTTP response. However, that's less readable and maintainable,
             # and a bit of delay isn't a problem given the slowness
             # of the Census downloads in the first place.
-            with open(filename, 'wb') as f:
+            with open(filename, "wb") as f:
                 f.write(response.content)
-            with zipfile.ZipFile(filename, 'r') as f:
-                f.extractall('./data')
-        elif (response.status_code == 404 and state.abbr == 'DC' and chamber == 'l') or \
-                (response.status_code == 404 and state.abbr == 'NE' and chamber == 'l'):
+            with zipfile.ZipFile(filename, "r") as f:
+                f.extractall("./data")
+        elif (
+            response.status_code == 404 and state.abbr == "DC" and chamber == "l"
+        ) or (response.status_code == 404 and state.abbr == "NE" and chamber == "l"):
             # These chambers are non-existant, and a `404` is expected
             pass
         else:
