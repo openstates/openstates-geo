ce9e70e3c229896d0db3cb9aea1b2adfca6f2890
James Turk <dev@jamesturk.net>
2020-04-16 14:21:55 -0400

    Multipolygon fix for layer mapping

--- djapp/geo/management/commands/load_divisions.py
+++ djapp/geo/management/commands/load_divisions.py
@@ -1,6 +1,7 @@
+import glob
 from django.core.management.base import BaseCommand
 from django.contrib.gis.gdal import DataSource
-from django.contrib.gis.utils import LayerMapping
+from django.contrib.gis.utils import LayerMapping, LayerMapError
 from ...models import DivisionSet, Division
 
 
@@ -12,13 +13,25 @@ GEOJSON_MAPPING = {
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("--verbose", action="store_true")
+        parser.add_argument("filenames", nargs="*")
+
     def handle(self, *args, **options):
         # create divisionsets
         DivisionSet.objects.get_or_create(slug="sldl")
         DivisionSet.objects.get_or_create(slug="sldu")
 
-        filename = "data/tl_2019_01_sldl-with-ocdids.geojson"
-        source = DataSource(filename)
+        filenames = options["filenames"] or sorted(glob.glob("final-geojson/*.geojson"))
 
-        lm = LayerMapping(Division, source, GEOJSON_MAPPING)
-        lm.save(progress=True)
+        for filename in filenames:
+            print(f"processing {filename}...")
+            source = DataSource(filename)
+            try:
+                lm = LayerMapping(Division, source, GEOJSON_MAPPING)
+            except LayerMapError:
+                # this error gets triggered if the GeoJSON type is MULTIPOLYGON
+                multipoly_mapping = GEOJSON_MAPPING.copy()
+                multipoly_mapping["shape"] = "MULTIPOLYGON"
+                lm = LayerMapping(Division, source, multipoly_mapping)
+            lm.save(verbose=options["verbose"])
