7807b011bb38932d7372e2eb73666736cddf3c78
James Turk <dev@jamesturk.net>
2021-06-23 14:24:16 -0400

    US support in to-geojson

--- scripts/to-geojson.py
+++ scripts/to-geojson.py
@@ -12,22 +12,35 @@ OCD_FIXES = {
     "ocd-division/country:us/state:vt/sldu:grand_isle-chittenden": "ocd-division/country:us/state:vt/sldu:grand_isle"
 }
 
+SKIPPED_GEOIDS = {
+    "cd-6098": "American Samoa",
+    "cd-6998": "Northern Mariana Islands",
+    "cd-6698": "Guam",
+    "cd-7898": "Virgin Islands",
+}
+
 
 def get_ocdid_records():
-    SLDU_CSV_PATH = "./data/us_sldu.csv"
-    with open(SLDU_CSV_PATH, "r") as sldu_file:
-        reader = csv.DictReader(sldu_file)
-        sldu = [row for row in reader]
-
-    SLDL_CSV_PATH = "./data/us_sldl.csv"
-    with open(SLDL_CSV_PATH, "r") as sldl_file:
-        reader = csv.DictReader(sldl_file)
-        sldl = [row for row in reader]
-
-    return sldu + sldl
+    paths = [
+        "./data/ocdids/us_sldu.csv",
+        "./data/ocdids/us_sldl.csv",
+        "./data/ocdids/us_cd.csv",
+    ]
+    all_divs = []
+    for path in paths:
+        with open(path, "r") as div_file:
+            reader = csv.DictReader(div_file)
+            all_divs += [row for row in reader]
+    return all_divs
 
 
 ocd_ids = get_ocdid_records()
+
+MTFCC_MAPPING = {
+    "G5200": "cd",
+    "G5210": "sldu",
+    "G5220": "sldl",
+}
 
 
 def merge_ids(geojson_path):
@@ -35,17 +48,22 @@ def merge_ids(geojson_path):
         geojson = json.load(geojson_file)
 
     for feature in geojson["features"]:
-        district_type = "sldu" if feature["properties"]["MTFCC"] == "G5210" else "sldl"
+        district_type = MTFCC_MAPPING[feature["properties"]["MTFCC"]]
 
         # Identify the OCD ID by making a lookup against the CSV files
         # The OCD ID is the cannonical identifier of an area on
         # the Open States platform
         geoid = "{}-{}".format(district_type, feature["properties"]["GEOID"])
+
+        if geoid in SKIPPED_GEOIDS:
+            continue
+
         for row in ocd_ids:
-            if row["census_geoid_14"] == geoid:
+            if row["census_geoid"] == geoid:
                 ocd_id = row["id"]
                 break
         else:
+            print(feature["properties"])
             raise AssertionError("Could not find OCD ID for GEOID {}".format(geoid))
 
         # Although OCD IDs contain the state postal code, parsing
@@ -55,19 +73,27 @@ def merge_ids(geojson_path):
         state_meta = metadata.lookup(abbr=state)
         if ocd_id in OCD_FIXES:
             ocd_id = OCD_FIXES[ocd_id]
-        district = state_meta.lookup_district(ocd_id)
-        if not district:
-            raise ValueError(f"no {ocd_id} {district_type}")
-            continue
+
+        if district_type == "cd":
+            cd_num = feature["properties"]["CD116FP"]
+            district_name = f"{state.upper()}-{cd_num}"
+        else:
+            district = state_meta.lookup_district(ocd_id)
+            district_name = district.name
+            if not district:
+                raise ValueError(f"no {ocd_id} {district_type}")
 
         feature["properties"] = {
             "ocdid": ocd_id,
-            "type": "sldl",
+            "type": district_type,
             "state": state,
-            "name": district.name,
+            "name": district_name,
         }
 
-    output_filename = f"data/geojson/{state}-{district_type}.geojson"
+    if district_type == "cd":
+        output_filename = f"data/geojson/us-{district_type}.geojson"
+    else:
+        output_filename = f"data/geojson/{state}-{district_type}.geojson"
     print(f"{geojson_path} => {output_filename}")
     with open(output_filename, "w") as geojson_file:
         json.dump(geojson, geojson_file)
@@ -114,7 +140,7 @@ if __name__ == "__main__":
     except FileExistsError:
         pass
 
-    expected = 101
+    expected = 102
     if len(sys.argv) == 1:
         files = sorted(glob.glob("data/source/tl*.shp"))
         if len(files) != expected:
@@ -123,7 +149,6 @@ if __name__ == "__main__":
         files = sys.argv[1:]
 
     process_va_lower("data/source/va_lower_remedial_2019.shp")
-    1/0
 
     for file in files:
         newfilename = file.replace(".shp", ".geojson")
@@ -135,7 +160,7 @@ if __name__ == "__main__":
                 [
                     "ogr2ogr",
                     "-where",
-                    "GEOID NOT LIKE '%ZZZ'",
+                    "GEOID NOT LIKE '%ZZ'",
                     "-t_srs",
                     "crs:84",
                     "-f",
