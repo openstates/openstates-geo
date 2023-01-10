729519a62bd44be38472715b0bda4fdb950e3125
John Seekins <john@civiceagle.com>
2023-01-10 09:55:30 -0700

    use redistricting hub data (manually downloaded and extracted)
    Signed-off-by: John Seekins <john@civiceagle.com>

--- scripts/to-geojson.py
+++ scripts/to-geojson.py
@@ -12,32 +12,33 @@ import yaml
 
 from utils import JURISDICTION_NAMES, ROOTDIR, setup_source, load_settings
 
+POSSIBLE_KEYS = ["DISTRICT", "SEN_DIST"]
 
-def _tiger_geoid(geojson, settings, geojson_path):
-    """
-    Process a GeoJSON file sent from TIGER.
-    """
+def merge_ids(geojson_path: str, settings: dict):
+    print(f"Converting IDs for {geojson_path}...")
+    with open(geojson_path, "r") as f:
+        geojson = json.load(f)
+    print(f"{len(geojson['features'])} features in {geojson_path}")
+    folder = geojson_path.split("/")[-2]
+    state, district_type = folder.split("_")[:2]
+    state_meta = metadata.lookup(abbr=state)
+    state_name = state_meta.name
+    state_fips = state_meta.fips
     for feature in geojson["features"]:
         print(f"Processing {feature['properties']}")
-        mtfcc = feature["properties"].get("MTFCC")
-        geoid = feature["properties"].get("GEOID")
-        # some files have weird additional ids all made of Zs. Skip them.
-        if "ZZ" in geoid:
-            continue
-        district_type = settings["MTFCC_MAPPING"][mtfcc]
-
-        geoid = f"{district_type}-{geoid}"
+        for key in POSSIBLE_KEYS:
+            district_id = feature["properties"].get(key, None)
+            if district_id:
+                break
+        district_padding = "0" * (3 - len(district_id))
+        geoid = f"{district_type}-{state_fips}{district_padding}{district_id}"
         if geoid in settings["SKIPPED_GEOIDS"]:
             continue
-
-        state = us.states.lookup(feature["properties"]["STATEFP"]).abbr.lower()
-        state_name = us.states.lookup(feature["properties"]["STATEFP"]).name
-        state_meta = metadata.lookup(abbr=state)
 
         """
         So we first get our mappings for the current jurisdiction
         Then we compare mappings.
-        Custom mappings must come first, or we may end up with in-accurate
+        Custom mappings must come first, or we may end up with inaccurate
         associations.
         And we should only check regex matching if custom mappings fail.
         """
@@ -55,7 +56,7 @@ def _tiger_geoid(geojson, settings, geojson_path):
             # cleanest way to strip leading 0s
             if mapping_type.get("match_type", "int") == "int":
                 dist_id = int(dist_id)
-            ocd_id = f"{mapping_type['os-id-prefix']}{dist_id}"
+            ocd_id = f"{mapping_type['os-id-prefix']}{dist_id}".lower()
 
         if district_type == "cd":
             cd_num = feature["properties"]["CD116FP"]
@@ -88,112 +89,6 @@ def _tiger_geoid(geojson, settings, geojson_path):
         json.dump(geojson, geojson_file)
 
 
-def _arp_geoid(geojson, settings, geojson_path):
-    """
-    convert data from the ARP
-    """
-    ocd_template = "ocd-division/country:us/state:{}/{}:{}"
-    for feature in geojson["features"]:
-        print(f"Processing: {feature['properties']}")
-        """
-        Keys can come in all caps _or_ all lowercase
-        handle both posiblities
-        """
-        try:
-            state_id = feature["properties"]["STA"]
-        except Exception:
-            state_id = feature["properties"]["sta"]
-        try:
-            type_abbr = feature["properties"]["TYPE_ABB"]
-        except Exception:
-            type_abbr = feature["properties"]["type_abb"]
-        try:
-            district_id = feature["properties"]["LABEL"]
-        except Exception:
-            district_id = feature["properties"]["label"]
-        try:
-            district_id = str(int(district_id))
-        except Exception:
-            pass
-        # be explicit here to avoid "false-y" values like 0
-        if district_id is None:
-            continue
-        us_state = us.states.lookup(state_id)
-        state_abbr = us_state.abbr.lower()
-        os_state_meta = metadata.lookup(abbr=state_abbr)
-        district_type = settings["ARP_DIST_MAPPING"][type_abbr]
-        mappings = settings["jurisdictions"][us_state.name]["id-mappings"]
-        ocd_id = None
-        for mapping in mappings.get("custom", []):
-            if mapping.get("arp-id", "") == district_id:
-                ocd_id = mapping["os-id"]
-                break
-        if not ocd_id:
-            if district_type == "cd" and district_id == "AL":
-                ocd_id = ocd_template.format(
-                    state_abbr, district_type, "at-large"
-                ).lower()
-            else:
-                ocd_id = ocd_template.format(
-                    state_abbr, district_type, district_id
-                ).lower()
-
-        # check for congressional district, formatting is slightly different
-        if district_type == "cd":
-            # pad a leading 0 for initial districts
-            if len(district_id) < 2:
-                district_id = f"0{district_id}"
-            district_name = f"{state_abbr.upper()}-{district_id}"
-        else:
-            district = os_state_meta.lookup_district(ocd_id)
-            if not district:
-                print(f"State meta: {os_state_meta}")
-                raise ValueError(f"no {ocd_id} {district_type}")
-            district_name = district.name
-
-        feature["properties"] = {
-            "ocdid": ocd_id,
-            "type": district_type,
-            "state": state_abbr,
-            "name": district_name,
-        }
-        print(f"Result: {feature['properties']}")
-    output_filename = f"data/geojson/{state_abbr}-{district_type}.geojson"
-    print(f"{geojson_path} => {output_filename}")
-    with open(output_filename, "w") as geojson_file:
-        json.dump(geojson, geojson_file)
-
-
-def merge_ids(geojson_path: str, meta_file: str, settings: dict):
-    print(f"Converting IDs for {geojson_path}...")
-    with open(geojson_path, "r") as f:
-        geojson = json.load(f)
-    prefix = geojson_path.rsplit(".", 1)[0]
-    meta_path = f"{prefix}_meta.geojson"
-    metajson = None
-    if os.path.exists(meta_path):
-        with open(meta_path, "r") as f:
-            metajson = json.load(f)
-    elif os.path.exists(meta_path.lower()):
-        meta_path = meta_path.lower()
-        with open(meta_path, "r") as f:
-            metajson = json.load(f)
-
-    """
-    First, check for TIGER-style setup
-    """
-    mtfcc = geojson["features"][0]["properties"].get("MTFCC", None)
-    geoid = geojson["features"][0]["properties"].get("GEOID", None)
-    if mtfcc and geoid:
-        _tiger_geoid(geojson, settings, geojson_path)
-    else:
-        if metajson and not geojson["features"][0]["properties"]:
-            print(f"Adding metadata from {meta_path} to {geojson_path}")
-            for geo_feat, meta_feat in zip(geojson["features"], metajson["features"]):
-                geo_feat["properties"] = meta_feat["properties"]
-        _arp_geoid(geojson, settings, geojson_path)
-
-
 if __name__ == "__main__":
     setup_source()
     SETTINGS = load_settings(f"{ROOTDIR}/configs")
@@ -207,7 +102,7 @@ if __name__ == "__main__":
 
         newfilename = file.replace(".shp", ".geojson")
         if os.path.exists(newfilename):
-            # print(f"{newfilename} already exists, skipping")
+            print(f"{newfilename} already exists, skipping")
             pass
         else:
             print(f"{file} => {newfilename}")
@@ -223,51 +118,4 @@ if __name__ == "__main__":
                 ],
                 check=True,
             )
-        """
-        Why name things consistently?
-        There maybe a dbf file with all lowercase
-        _or_ casing consistent with the shp file
-        """
-        meta_file = file.replace(".shp", ".dbf")
-        new_meta = meta_file.replace(".dbf", "_meta.geojson")
-        meta_file_lower = meta_file.lower()
-        new_meta_lower = new_meta.lower()
-        if os.path.exists(meta_file):
-            if os.path.exists(new_meta):
-                # print(f"{new_meta} already exists, skipping")
-                pass
-            else:
-                print(f"{meta_file} => {new_meta}")
-                subprocess.run(
-                    [
-                        "ogr2ogr",
-                        "-t_srs",
-                        "crs:84",
-                        "-f",
-                        "GeoJSON",
-                        new_meta,
-                        meta_file,
-                    ],
-                    check=True,
-                )
-        elif os.path.exists(meta_file_lower):
-            meta_file = meta_file_lower
-            new_meta = new_meta_lower
-            if os.path.exists(new_meta_lower):
-                # print(f"{new_meta_lower} already exists, skipping")
-                pass
-            else:
-                print(f"{meta_file_lower} => {new_meta_lower}")
-                subprocess.run(
-                    [
-                        "ogr2ogr",
-                        "-t_srs",
-                        "crs:84",
-                        "-f",
-                        "GeoJSON",
-                        new_meta_lower,
-                        meta_file_lower,
-                    ],
-                    check=True,
-                )
-        merge_ids(newfilename, new_meta, SETTINGS)
+        merge_ids(newfilename, SETTINGS)
