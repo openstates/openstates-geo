dac53a236b44323fda33377e748bfce149fa4fab
John Seekins <john@civiceagle.com>
2023-01-12 15:20:14 -0700

    re-order script steps
    Signed-off-by: John Seekins <john@civiceagle.com>

--- make-tiles.sh
+++ make-tiles.sh
@@ -17,9 +17,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ###
 # Following README instructions
 ###
-poetry run python "${SCRIPT_DIR}/scripts/get-shapefiles.py"
+# poetry run python "${SCRIPT_DIR}/scripts/get-shapefiles.py"
 poetry run python "${SCRIPT_DIR}/scripts/to-geojson.py"
+poetry run python "${SCRIPT_DIR}/scripts/make-tiles.py"
 poetry run python "${SCRIPT_DIR}/manage.py" migrate
 poetry run python "${SCRIPT_DIR}/manage.py" load_divisions
 poetry run python "${SCRIPT_DIR}/scripts/update-boundaries.py"
-poetry run python "${SCRIPT_DIR}/scripts/make-tiles.py"
