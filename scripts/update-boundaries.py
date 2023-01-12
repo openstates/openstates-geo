3bde44e4307e1f75ee5b3fe58dae169cac6cda2b
John Seekins <john@civiceagle.com>
2023-01-12 13:41:56 -0700

    fix linting and skip existing files
    Signed-off-by: John Seekins <john@civiceagle.com>

--- scripts/update-boundaries.py
+++ scripts/update-boundaries.py
@@ -15,9 +15,13 @@ def main():
     for file in glob.glob(f"{ROOTDIR}/data/geojson/*.geojson"):
         with open(file, "r") as f:
             obj = json.load(f)
+        print(f"Processing {file} into district-level boundary files")
         for feature in obj["features"]:
             metadata = dict(feature["properties"])
             folder, filename = metadata["ocdid"].rsplit("/", 1)
+            full_path = f"{ROOTDIR}/data/boundaries/{folder}/{filename}.json"
+            if os.path.exists(full_path):
+                print(f"{filename}.json already exists. Skipping.")
             os.makedirs(f"{ROOTDIR}/data/boundaries/{folder}", exist_ok=True)
             obj = {
                 "shape": feature["geometry"],
@@ -29,7 +33,7 @@ def main():
             }
             with open(f"{ROOTDIR}/data/boundaries/{folder}/{filename}.json", "w") as f:
                 json.dump(obj, f)
-            exit(1)
+
 
 if __name__ == "__main__":
     main()
