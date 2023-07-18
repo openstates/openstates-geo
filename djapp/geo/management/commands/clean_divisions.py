225d15f6bd1c0049a719398360f6c91947e14d74
John Seekins <john@pluralpolicy.com>
2023-07-18 13:03:34 -0600

    update exceptions and actually delete
    Signed-off-by: John Seekins <john@pluralpolicy.com>

--- djapp/geo/management/commands/clean_divisions.py
+++ djapp/geo/management/commands/clean_divisions.py
@@ -9,19 +9,20 @@ ROOTDIR = Path(__file__).parent.parent.parent.parent.parent.absolute()
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        print("Checking for any divisions we should remove...")
         ocd_ids = []
         for filename in glob.glob(f"{ROOTDIR}/data/geojson/*.geojson"):
             obj = json.load(open(filename, "r"))
             ocd_ids.extend(div["properties"]["ocdid"] for div in obj["features"])
         print(f"Loaded {len(ocd_ids)} local divisions for comparison")
         if len(ocd_ids) < 1:
-            raise Exception("Didn't find any local divisions.")
+            raise Exception("No local divisions found")
 
         to_delete = [div for div in Division.objects.exclude(id__in=ocd_ids)]
 
         print(f"Found {len(to_delete)} divisions not stored locally")
         if len(to_delete) > len(ocd_ids):
-            raise Exception("Found more objects to delete than expected objects.")
+            raise Exception("Found more objects to delete than expected objects")
 
         # delete command to remove old divisions
-        # Division.objects.exclude(id__in=ocd_ids).delete()
+        Division.objects.exclude(id__in=ocd_ids).delete()
